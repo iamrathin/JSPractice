d85280004fe607498b61b1bd5bc2bf516bcb4b1d
Rathin Sharma <iamrathinsharma>
2019-12-25 14:09:09 +0530

    day1 commit 1

---
+++ JS_Sandbox/app.js
@@ -0,0 +1,86 @@
+/*
+
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greeting ='Hello';
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+console.error('This is some error');
+console.clear();
+console.warn('This is a warning');
+console.time('Hello');
+    console.log('Hello');
+    console.log('Hello');
+    console.log('Hello');
+    console.log('Hello');
+    console.log('Hello');
+    console.log('Hello');
+console.timeEnd('Hello');
+
+*/
+
+
+/*
+// var, let, const
+
+var name ='John Doe';
+console.log(name);
+name='Steve Smith';
+console.log(name);
+
+//init var
+
+var greeting;
+console.log(greeting);
+greeting='Hello';
+console.log(greeting);
+
+
+//letters numbers, _, $
+//can not start with number
+
+
+//multi word vars
+
+var firstName ='John'; //camel case, JS, Java
+var first_name ='Sara'; //underscoring, php
+var FirstName ='Tom'; //Pascal case
+var firstname;
+
+
+*/
+
+
+/*
+let name ='John Doe';
+console.log(name);
+name='Steve Smith';
+console.log(name);
+*/
+
+//Const
+
+//const name ='John'
+//console.log(name);
+
+//can not reassign
+// name ='Sara';
+//have to assign a value
+// const greeting;
+
+/*
+const person = {
+    name: 'John',
+    age:30
+}
+person.name='Sara';
+person.age=43;
+console.log(person);
+*/
+
+const numbers= [1,2,3,4,5];
+numbers.push(6);
+console.log(numbers);
